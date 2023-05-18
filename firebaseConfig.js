523019ed0b02ac235aa6e23814377b4f3723ca37
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2023-05-18 13:28:33 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,14 +6,15 @@ import { getStorage } from "firebase/storage";
 
 import { GoogleAuthProvider } from "firebase/auth";
 
+// AAHAR  API
 const firebaseConfig = {
-  apiKey: "AIzaSyACEksh0hlO9FQUAy3IjQkCwe7Cof5Il1U",
+  apiKey: "",
   authDomain: "aahar-109e8.firebaseapp.com",
   projectId: "aahar-109e8",
   storageBucket: "aahar-109e8.appspot.com",
-  messagingSenderId: "889513020046",
-  appId: "1:889513020046:web:444d7956731ad698f597df",
-  measurementId: "G-1X3J5XKQFN",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
